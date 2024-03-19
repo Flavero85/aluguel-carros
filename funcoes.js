962cc9ace5caf3af8c8ae1d00fd01c4057315d72
FLAVIO RAFAEL CARDOZO <flavero@gmail.com>
2024-03-18 22:23:39 -0300

    Update funcoes.js

--- funcoes.js
+++ funcoes.js
@@ -2,5 +2,11 @@ function Trocar(){
     let tipo = document.getElementById("veiculo").value
     alert(tipo)
 
-    
+    if(tipo == "Básico"){
+        document.getElementById("imagem").src="basico.png"
+    }else if{
+        document.getElementById("imagem").src="esporte.png"
+    }else{
+        document.getElementById("imagem").src="completo.png"
+    }
 }
