00eb74d1c4e17b689aee02b8cb82b19c86e6dd0c
FLAVIO RAFAEL CARDOZO <flavero@gmail.com>
2024-03-22 21:34:21 -0300

    up

--- listar.php
+++ listar.php
@@ -1,13 +1,26 @@
 <?php
 $nomearquivo = "clientes.txt";
 $arquivo = fopen($nomearquivo, "r");
+
+echo "<table border=1>";
+echo "<tr style='color: red;'>";
+echo "<td><strong>Nome</strong></td>";
+echo "<td><strong>Email</strong></td>";
+echo "</tr>";
+
 while(!feof($arquivo)){
     $linha = fgets($arquivo, 1000);
     if ($linha != ""){
-        echo "<li>$linha</li>";
+        $conteudo = explode(";", $linha);
+        echo "<tr>";
+        echo "<td>$conteudo[0]</td>";
+        echo "<td>$conteudo[1]</td>";
+        echo "</tr>";
+        
     }
 }
-
+echo "</table>";
 fclose($arquivo);
 
 ?>
+
